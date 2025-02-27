d6ef9134ca3c792d13f16e11da0e5f28daecd226
MohamedTwahir <mohamedtwahir977@gmail.com>
2025-02-26 19:20:06 -0600

    while_loop concept

---
+++ low-level_languages/C/C_concepts/while_loop.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+* main - this function demonstrates the usage of while loop in code execution.
+* dig - refers to digit/number used in demonstration of the while loop. 
+*/
+
+int main(){
+
+    // variable definition
+    int dig = 1;
+
+    // while loop example
+    while(dig <= 6){
+        printf("This is the while loop executing\n");
+        dig++;
+
+    }
+    printf("The loop has ended\n");
+    return 0;
+}
