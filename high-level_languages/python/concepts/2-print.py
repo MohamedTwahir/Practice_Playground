05901e86652c96dce20ddf06c67180a93b5e2ca6
MohamedTwahir <mohamedtwahir977@gmail.com>
2025-05-24 19:35:57 -0500

    Hello, print

---
+++ high-level_languages/python/concepts/2-print.py
@@ -0,0 +1,4 @@
+# A Python script that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
+#!/usr/bin/env python3
+#!/usr/bin/env python3
+print('"Programming is like building a multilingual puzzle')
