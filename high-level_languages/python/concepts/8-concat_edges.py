2d152b0190fcb32e7f3b8a0d0d958ba97de5c1dd
MohamedTwahir <mohamedtwahir977@gmail.com>
2025-05-25 19:45:25 -0500

    Create a new sentence

---
+++ high-level_languages/python/concepts/8-concat_edges.py
@@ -0,0 +1,6 @@
+# python script that creates a new sentence by concatenating an existing sentence
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str[39:66] + " " + "with Python")
+
