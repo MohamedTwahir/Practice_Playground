2b985db6bb6760eefee15511dd5578c98b693476
MohamedTwahir <mohamedtwahir977@gmail.com>
2025-05-28 19:39:06 -0500

    sum of numbers in a list

---
+++ high-level_languages/python/concepts/sum.py
@@ -0,0 +1,6 @@
+# A python script that calculates sum of all numbers in a list using for loop
+numbers = [1, 5, 3, 9]
+total = 0
+for number in numbers:
+    total = total + number
+print(f"The sum of the numbers in the list is {total}") 
